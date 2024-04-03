6095d5fd365b81118de75ecbe7ced816dde183f4
Tessa5 <ktessa262@naver.com>
2024-04-03 11:54:58 +0300

    implemented clerk webhooks

--- .history/middleware_20240403113833.ts
+++ .history/middleware_20240403113833.ts
@@ -5,7 +5,7 @@ export default authMiddleware({
   publicRoutes: ['/api/webhooks/clerk'],
   // Routes that can always be accessed, and have
   // no authentication information
-  
+  ,
 });
  
 export const config = {
